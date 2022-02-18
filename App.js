dc03db2fb0dc8699df1a231c95f90e99ffa8f715
Rimaro <leonardo.ongaro2003@hotmail.com>
2022-02-18 21:37:49 +0100

    continued

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import reactDom from 'react-dom';
-import { StyleSheet, Text, View, SectionList, TextInput} from 'react-native';
-import {useState} from 'react'
+import { StyleSheet, Text, View, SectionList, TextInput, Image } from 'react-native';
+import { useState } from 'react'
 import { Button, ButtonGroup, withTheme } from 'react-native-elements';
 
 const todoListJSON = require("./todo.json")
 
 const TodoList = (props) => {
-    const [todoList, setTodoList] = useState(props.list.lists)
+    const [todoList, setTodoList] = useState(props.list)
 
     const changeTaskStatus = (task) => {
+        console.log(props)
         todoList.completedTasks.push(<Text style={styles.completedTask}>{task}</Text>)
-        todoList.todoList.splice(todoList.todoList.indexOf(task), 1)
+        todoList.lists.splice(todoList.lists.indexOf(task), 1)
     }
 
     return (
@@ -20,11 +21,38 @@ const TodoList = (props) => {
             sections={props.list.lists}
             renderItem={({ item }) => (
                 <Text style={styles.item} onPress={() => changeTaskStatus(item)}>
+                    <Image source={require('./assets/unchecked.png')} style={styles.checkbox} />
                     {item}
                 </Text>
             )}
             keyExtractor={(item, index) => index}
         />
+
+    );
+};
+
+const CompletedTasks = (props) => {
+    const [todoList, setTodoList] = useState(props.list)
+
+    const changeTaskStatus = (task) => {
+        console.log(props)
+        /*todoList.completedTasks.push(<Text style={styles.completedTask}>{task}</Text>)
+        todoList.lists.splice(todoList.lists.indexOf(task), 1)*/
+    }
+
+    return (
+        <SectionList
+            style={styles.list}
+            sections={props.list.CompletedTasks}
+            renderItem={({ item }) => (
+                <Text style={styles.item} onPress={() => changeTaskStatus(item)}>
+                    <Image source={require('./assets/checked.png')} style={styles.checkbox} />
+                    {item}
+                </Text>
+            )}
+            keyExtractor={(item, index) => index}
+        />
+
     );
 };
 
@@ -33,6 +61,8 @@ export default function App() {
         <View style={styles.container}>
             <Text style={styles.title}>Todo List:</Text>
             <TodoList list={todoListJSON} />
+            <Text style={styles.title}>Completed tasks:</Text>
+            <CompletedTasks list={todoListJSON} />
         </View>
     );
 }
@@ -41,5 +71,20 @@ const styles = StyleSheet.create({
     container: {
         marginTop: 50,
         width: '100%',
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        padding: 10,
+
+    },
+    item: {
+        margin: 6,
+        fontFamily: 'Roboto',
+        padding: 5,
+        fontSize: 18,
+        height: 44,
+        backgroundColor: "rgb(51,51,51)",
     },
 });
